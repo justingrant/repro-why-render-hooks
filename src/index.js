10fe005f97d982e1d9ce233d2379a9587e71fd22
Justin Grant <justin@justingrant.net>
2019-12-20 22:52:12 -0800

    simplified repro

--- src/index.js
+++ src/index.js
@@ -4,14 +4,11 @@ import ReactDOM from "react-dom";
 const whyDidYouRender = require("@welldone-software/why-did-you-render");
 whyDidYouRender(React);
 
-function App() {
-  return <Repro />;
-}
-
 function Repro() {
   const [count] = useState(0);
   const obj = useMemo(() => ({ count }), [count]);
   return <div>count: {obj.count}</div>;
 }
 
+const App = () => <Repro />;
 ReactDOM.render(<App />, document.getElementById("root"));
